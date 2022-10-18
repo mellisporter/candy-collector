ecb0a426a934ccdd5c7b695fad63fcd750f4984a
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-18 11:35:36 -0400

    added about route

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 #define the home view
 def home(request):
     return HttpResponse('Hello world, this is the candy app!')
+
+def about(request):
+    return HttpResponse("This is the about page")
