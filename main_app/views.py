c6a28c3bbbddbf2ede74def75d5b1aeb3a865ee2
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-18 13:51:33 -0400

    psql datatbase connected

--- main_app/views.py
+++ main_app/views.py
@@ -3,18 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# import 
+from main_app.models import Candy
+
 # MODELS
 
-class Candy:
-    def __init__(self, name, house, description):
-        self.name= name
-        self.house= house
-        self.description = description
+# class Candy:
+#     def __init__(self, name, house, description):
+#         self.name= name
+#         self.house= house
+#         self.description = description
 
-candy = [
-    Candy('Twizzlers' , 'The Porters' , 'Red and chewy'),
-    Candy('Reeses', 'The Millers' , 'The best there is.')
-]
+# candy = [
+#     Candy('Twizzlers' , 'The Porters' , 'Red and chewy'),
+#     Candy('Reeses', 'The Millers' , 'The best there is.')
+# ]
 
 #define the home view
 def home(request):
@@ -24,5 +27,6 @@ def about(request):
     return render(request, 'about.html')
 
 def candy_index(request):
+    candy = Candy.objects.all()
     return render(request, 'candy/index.html' , {'candy' : candy})
 
