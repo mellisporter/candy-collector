d4a56b243c214ee640a503ee9e00aef33300ce85
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-18 12:06:34 -0400

    renders candy list index

--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# MODELS
+
+class Candy:
+    def __init__(self, name, house, description):
+        self.name= name
+        self.house= house
+        self.description = description
+
+candy = [
+    Candy('Twizzlers' , 'The Porters' , 'Red and chewy'),
+    Candy('Reeses', 'The Millers' , 'The best there is.')
+]
+
 #define the home view
 def home(request):
     return HttpResponse('Hello world, this is the candy app!')
 
 def about(request):
     return render(request, 'about.html')
+
+def candy_index(request):
+    return render(request, 'candy/index.html' , {'candy' : candy})
+
