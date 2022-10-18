6f336303bdd5039af5c5e55c94ee1dcd2d2256ad
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-18 11:33:08 -0400

    url setup complete and home page returns

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+#define the home view
+def home(request):
+    return HttpResponse('Hello world, this is the candy app!')
