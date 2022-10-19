1d55a3a96ae8926a66183c6dd4589ad52f583746
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-19 12:01:24 -0400

    add second model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+
 class Candy(models.Model):
     name = models.CharField(max_length=100)
     house = models.CharField(max_length=100)
@@ -13,3 +15,12 @@ class Candy(models.Model):
     # Add this method
     def get_absolute_url(self):
         return reverse('detail', kwargs={'candy_id': self.id})
+
+class Ingredients(models.Model):
+    ing1= models.CharField(max_length=100)
+    exp_date= models.DateField()
+
+    candy= models.ForeignKey(Candy, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ing1
